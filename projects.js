56cfc219f53ebeb9bb12ccc124f5c48f7d189f6e
Dan Dan <wunna6740@gmail.com>
2026-09-13 02:53:22 +0700

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,7 +1,27 @@
 const initialProjects = [
     {
-        "id": "1789239796514",
+        "id": "1789241110379",
         "title": "Albert Prize",
+        "date": "2026-09-10",
+        "tags": [
+            "MC & Hosting",
+            "Others"
+        ],
+        "desc": "Took on as the role of MC at the 16th Albert Prize while being a finalist for 2 award categories<div><ol><li>MC of the Year</li><li>Student of the Year</li></ol></div>",
+        "images": [
+            "https://i.postimg.cc/D0FfVqWN/AP1.jpg",
+            "https://i.postimg.cc/SRrSmJ8g/AP2.jpg",
+            "https://i.postimg.cc/GtQLbHYM/AP3.jpg",
+            "https://i.postimg.cc/y6XYmr5p/AP4.jpg",
+            "https://i.postimg.cc/c1M4R5jb/AP5.jpg",
+            "https://i.postimg.cc/65W6YfxB/AP6.jpg",
+            "https://i.postimg.cc/WbNpXwcT/AP7.jpg"
+        ],
+        "video": ""
+    },
+    {
+        "id": "1789239796514",
+        "title": "Albert Prize Finalist Nomination",
         "date": "2026-08-30",
         "tags": [
             "MC & Hosting",
@@ -15,5 +35,41 @@ const initialProjects = [
             "https://i.postimg.cc/cLGCCmwP/Screenshot-2026-09-10-at-00-43-09.png"
         ],
         "video": ""
+    },
+    {
+        "id": "1789241678176",
+        "title": "MC of AU Workshop 2026",
+        "date": "2026-08-15",
+        "tags": [
+            "MC & Hosting"
+        ],
+        "desc": "Asked to be MC for the MC of AU Workshop 2026<div><ul><li>helped training the essential skills needed to be a MC</li><li>shared experience of being an event MC&nbsp;</li></ul></div>",
+        "images": [
+            "https://i.postimg.cc/HnG0LM2h/9D702E7A-603D-4754-9D0A-7F8EAF02CC3A.jpg",
+            "https://i.postimg.cc/5NVqdwjw/A1-00156.jpg",
+            "https://i.postimg.cc/Kv2rSn4P/A1-00161.jpg",
+            "https://i.postimg.cc/Fs4g5j1V/A1-00257.jpg"
+        ],
+        "video": ""
+    },
+    {
+        "id": "1789241364109",
+        "title": "Strategic Leader Camp 2026 (SLC 2026)",
+        "date": "2026-07-18",
+        "tags": [
+            "MC & Hosting",
+            "Event Organizing"
+        ],
+        "desc": "<p><strong>Co-Project Director &amp; Reaction MC — AU SLC 2026</strong></p><p>Worked as a Co-Project Director and Reaction MC at SLC 2026, an event designed to educate different SAUs on essential knowledge, responsibilities, and procedures required when serving as committee members for AU activity units.</p>",
+        "images": [
+            "https://i.postimg.cc/4dQqvXS8/745623546-1062883289426375-178878765700098540-n.jpg",
+            "https://i.postimg.cc/Jn5v3R2d/751549794-1062868362761201-2054440923854571287-n-2.jpg",
+            "https://i.postimg.cc/pTJ4QP6S/751643437-1062888739425830-4507378523818819790-n.jpg",
+            "https://i.postimg.cc/FRV8bhBq/752488810-1062888322759205-7627467835588534706-n-2.jpg",
+            "https://i.postimg.cc/Hx9R0ThR/752609557-1062886022759435-5974769106039008049-n.jpg",
+            "https://i.postimg.cc/XJkmKnP6/752609796-1062893419425362-1512649908540421693-n.jpg",
+            "https://i.postimg.cc/ZRx2PJQS/SLC-134.jpg"
+        ],
+        "video": ""
     }
-];
+]
