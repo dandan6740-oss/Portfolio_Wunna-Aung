5eb3843cdd991a1a120ac37ae0cd682990eb2dd5
Dan Dan <wunna6740@gmail.com>
2026-09-13 03:39:25 +0700

    Update projects.js

--- projects.js
+++ projects.js
@@ -71,5 +71,104 @@ const initialProjects = [
             "https://i.postimg.cc/ZRx2PJQS/SLC-134.jpg"
         ],
         "video": ""
+    },
+    {
+        "id": "1789245372873",
+        "title": "AUSO 2026",
+        "date": "2026-07-10",
+        "tags": [
+            "Others"
+        ],
+        "desc": "Inauguration Ceremony for Student Leaders 2026<div>Academic Year 2026: Chairperson for International Affairs &amp; Networks</div>",
+        "images": [
+            "https://i.postimg.cc/xdmhM8gC/AG1.jpg",
+            "https://i.postimg.cc/G21NQRSL/AG2.jpg",
+            "https://i.postimg.cc/TwXS9xHb/AG3.jpg"
+        ],
+        "video": ""
+    },
+    {
+        "id": "1789243041034",
+        "title": "Home Away from Home: Myanmar to ABAC",
+        "date": "2026-02-12",
+        "tags": [
+            "MC & Hosting",
+            "Event Organizing"
+        ],
+        "desc": "One of the largest-scaled event from AUMSC 2025 Culture Event \"Home Away from Home: Myanmar to ABAC\"<div><ul><li>took on the role as MC&nbsp;</li><li>responsible for the PR of the event</li><li>able to obtain sponsorship from True dtac 5G, and other major business entities</li><li>involved in the process of project proposal to project closing</li></ul></div>",
+        "images": [
+            "https://i.postimg.cc/G2nJLYqR/Culture4.jpg",
+            "https://i.postimg.cc/4dRbftwM/Culture1.jpg",
+            "https://i.postimg.cc/76rMH7my/Culture2.jpg",
+            "https://i.postimg.cc/Y9BfrgbH/Culture3.jpg"
+        ],
+        "video": ""
+    },
+    {
+        "id": "1789244175472",
+        "title": "AUISC Halloween: The Witching Hour",
+        "date": "2025-11-27",
+        "tags": [
+            "MC & Hosting"
+        ],
+        "desc": "MC for AUISC Halloween: The Witching Hour, an event dedicated to halloween and students' costume creativity along with performances",
+        "images": [
+            "https://i.postimg.cc/7LjtL4yQ/Halloween1.jpg",
+            "https://i.postimg.cc/5tDKtJVJ/Halloween2.jpg",
+            "https://i.postimg.cc/QMvYMDrR/Halloween3.jpg",
+            "https://i.postimg.cc/fb1qbsNh/Halloween4.jpg"
+        ],
+        "video": ""
+    },
+    {
+        "id": "1789243313587",
+        "title": "AU Fun Run",
+        "date": "2025-11-12",
+        "tags": [
+            "MC & Hosting"
+        ],
+        "desc": "MC for AU Fun Run 2025: AU Zodiac Run",
+        "images": [
+            "https://i.postimg.cc/KvQ617K5/AU-663.jpg",
+            "https://i.postimg.cc/m2bxZprS/AU-593.jpg",
+            "https://i.postimg.cc/QxXLNYMz/AU-596.jpg",
+            "https://i.postimg.cc/fT5GV735/AU-639.jpg"
+        ],
+        "video": ""
+    },
+    {
+        "id": "1789243451971",
+        "title": "Digital Marketing Campaign: Warrix",
+        "date": "2025-09-30",
+        "tags": [
+            "Academic Project"
+        ],
+        "desc": "An academic project for a course called \"Digital Marketing\"<div><ul><li>Assigned brand: Warrix</li><li>Task: Create a 3-month digital campaign for Warrix</li></ul></div>",
+        "images": [
+            "https://i.postimg.cc/v8g3dryJ/Ending.png",
+            "https://i.postimg.cc/FFdDQyvB/Content-Idea.png",
+            "https://i.postimg.cc/h4zpqLgF/Content-Sample.png",
+            "https://i.postimg.cc/KckJyPbd/Influencer-Choice.png",
+            "https://i.postimg.cc/KckJyPbC/Timeline.png"
+        ],
+        "video": ""
+    },
+    {
+        "id": "1789243220686",
+        "title": "MC of AU 2025",
+        "date": "2025-08-14",
+        "tags": [
+            "MC & Hosting",
+            "Others"
+        ],
+        "desc": "MC of AU, an event where official MC for AU events are selected<div><ul><li>Selected as one of the MC of AU&nbsp;</li><li>Won \"Best Showcase\"</li></ul></div>",
+        "images": [
+            "https://i.postimg.cc/KzZjDrN5/AP-221.jpg",
+            "https://i.postimg.cc/0Q8j0dfW/AP-429.jpg",
+            "https://i.postimg.cc/4dBdL7L6/AP-505.jpg",
+            "https://i.postimg.cc/KzZjDrNs/AP-519.jpg",
+            "https://i.postimg.cc/tT2TSZSd/AP-539.jpg"
+        ],
+        "video": ""
     }
 ]
